d823b4e83f732bc4f2aee4307f4da2d40821f149
ehercor0612 <elias.heredia.daw2@gmail.com>
2023-02-10 12:24:42 +0100

    Gitignore cambiado, ignora .angular

--- Animaid.web/Controllers/WeatherForecastController.cs
+++ Animaid.web/Controllers/WeatherForecastController.cs
@@ -19,15 +19,9 @@ namespace Animaid.web.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public void Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+
         }
     }
 }
